d59e125cab31c0a8792fcb8fd9be203b2fe70738
Joeytat <joeyqaq@gmail.com>
2017-12-01 20:43:08 +0800

    Device - Add phone call method;

--- Sparrow/Classes/Extensions/Device+Extension.swift
+++ Sparrow/Classes/Extensions/Device+Extension.swift
@@ -10,36 +10,41 @@ import AudioUnit
 import AVFoundation
 
 public struct Device {
-  public static func playSound(_ filename: String, type: String? = nil) {
-    guard let filePath = Bundle.main.path(forResource: filename, ofType: type)
-      else { return }
-    let url = NSURL(fileURLWithPath: filePath)
-    var soundId: SystemSoundID = 0
-    AudioServicesCreateSystemSoundID(url, &soundId)
-    AudioServicesPlayAlertSound(soundId)
-  }
-  
-  public static func flashlight(_ isOn: Bool) {
-    if Device.isSimulator() { return }
-    guard let flashlight: AVCaptureDevice = AVCaptureDevice.default(for: .video)
-      else { return }
-    
-    if flashlight.isTorchAvailable && flashlight.isTorchModeSupported(.on) {
-      guard let _ = try? flashlight.lockForConfiguration() else {
-        return
-      }
-      flashlight.torchMode = isOn ? .on : .off
-      flashlight.unlockForConfiguration()
+    public static func playSound(_ filename: String, type: String? = nil) {
+        guard let filePath = Bundle.main.path(forResource: filename, ofType: type)
+            else { return }
+        let url = NSURL(fileURLWithPath: filePath)
+        var soundId: SystemSoundID = 0
+        AudioServicesCreateSystemSoundID(url, &soundId)
+        AudioServicesPlayAlertSound(soundId)
     }
-  }
-  
-  public static func isSimulator() -> Bool {
-    #if arch(i386) || arch(x86_64)
-      return true
-    #else
-      return false
-    #endif
-  }
-  
-  public static let isPad = UIDevice.current.userInterfaceIdiom == .pad
+    
+    public static func flashlight(_ isOn: Bool) {
+        if Device.isSimulator() { return }
+        guard let flashlight: AVCaptureDevice = AVCaptureDevice.default(for: .video)
+            else { return }
+        
+        if flashlight.isTorchAvailable && flashlight.isTorchModeSupported(.on) {
+            guard let _ = try? flashlight.lockForConfiguration() else {
+                return
+            }
+            flashlight.torchMode = isOn ? .on : .off
+            flashlight.unlockForConfiguration()
+        }
+    }
+    
+    public static func isSimulator() -> Bool {
+        #if arch(i386) || arch(x86_64)
+            return true
+        #else
+            return false
+        #endif
+    }
+    
+    public static let isPad = UIDevice.current.userInterfaceIdiom == .pad
+    
+    public static func makePhoneCall(_ number: String) {
+        guard let number = URL(string: "tel://" + number) else { return }
+        UIApplication.shared.openURL(number)
+    }
 }
