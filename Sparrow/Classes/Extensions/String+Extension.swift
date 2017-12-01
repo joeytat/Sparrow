a07d8e26bde1c1609dd21592e421b39b144dada1
Joeytat <joeyqaq@gmail.com>
2017-12-01 20:42:41 +0800

    String - Add text width & height calculator;

--- Sparrow/Classes/Extensions/String+Extension.swift
+++ Sparrow/Classes/Extensions/String+Extension.swift
@@ -29,3 +29,19 @@ public extension Optional where Wrapped == String {
         return !isNilOrEmpty
     }
 }
+
+
+public extension String {
+    public func widthWithConstrainedWidth(font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: font.lineHeight)
+        let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedStringKey.font: font], context: nil)
+        return boundingBox.width
+    }
+    
+    public func heightWithConstrainedWidth(width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedStringKey.font: font], context: nil)
+        return boundingBox.height
+    }
+}
+
