858905e6cd51452808a4799d95f84735d10b0b79
Joeytat <joeyqaq@gmail.com>
2017-12-15 17:12:43 +0800

    Add UIAlertController helper;

---
+++ Sparrow/Classes/Rx/UIAlertController+Rx.swift
@@ -0,0 +1,54 @@
+//
+//  UIAlertController+Rx.swift
+//  Alamofire
+//
+//  Created by Joey on 15/12/2017.
+//
+
+import Foundation
+import RxSwift
+
+public extension Reactive where Base: UIAlertController {
+    public static func presentInputAlert(by vc:UIViewController, title: String) -> Observable<String?> {
+        return Observable<String?>
+            .create { observer in
+                let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+                alert.addTextField(configurationHandler: { textField in
+                    textField.placeholder = "请输入\(title)"
+                    textField.becomeFirstResponder()
+                })
+                alert.addAction(UIAlertAction(title: "取消", style: .default, handler: { _ in
+                    observer.onNext(nil)
+                    observer.onCompleted()
+                }))
+                alert.addAction(UIAlertAction(title: "确定", style: .default, handler: { _ in
+                    let text = alert.textFields?.first!.text
+                    observer.onNext(text)
+                    observer.onCompleted()
+                }))
+                vc.present(alert, animated: true, completion: nil)
+                return Disposables.create {
+                    vc.dismiss(animated: true, completion: nil)
+                }
+        }
+    }
+    
+    public static func presentConfirmAlert(by vc: UIViewController, title: String) -> Observable<Bool> {
+        return Observable<Bool>
+            .create { observer in
+                let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "取消", style: .default, handler: { _ in
+                    observer.onNext(false)
+                    observer.onCompleted()
+                }))
+                alert.addAction(UIAlertAction(title: "确定", style: .default, handler: { _ in
+                    observer.onNext(true)
+                    observer.onCompleted()
+                }))
+                vc.present(alert, animated: true, completion: nil)
+                return Disposables.create {
+                    vc.dismiss(animated: true, completion: nil)
+                }
+        }
+    }
+}
