4fcfc174ab73f99187c78d0075f26bc058bc5bcc
Joeytat <joeyqaq@gmail.com>
2017-12-15 17:10:37 +0800

    String+Extension - Add few validation methods;

--- Sparrow/Classes/Extensions/String+Extension.swift
+++ Sparrow/Classes/Extensions/String+Extension.swift
@@ -45,3 +45,27 @@ public extension String {
     }
 }
 
+// MARK: - Validation
+public extension String {
+    public func matches(for regex: String) -> [String] {
+        do {
+            let regex = try NSRegularExpression(pattern: regex)
+            let nsString = self as NSString
+            let results = regex.matches(in: self, range: NSRange(location: 0, length: nsString.length))
+            return results.map { nsString.substring(with: $0.range)}
+        } catch let error {
+            print("invalid regex: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
+    public func isPhoneNumber(strict: Bool = false) -> Bool {
+        // TODO: - 补全严格校验
+        let match = matches(for: "^(13[0-9]|14[579]|15[0-3,5-9]|17[0135678]|18[0-9])\\d{0,8}$")
+        return match.count > 0
+    }
+    
+    public func isIdCard() -> Bool {
+        return self.matches(for: "^([1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9]|X))$").count > 0
+    }
+}
