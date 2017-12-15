db10ab553882e1eba3454302077e2635763146ba
Joeytat <joeyqaq@gmail.com>
2017-12-15 17:12:33 +0800

    Add Moya LoadingPlugin to display HUD;

---
+++ Moya Plugin/LoadingPlugin.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingPlugin.swift
+//  Alamofire
+//
+//  Created by Joey on 15/12/2017.
+//
+
+import Foundation
+import Moya
+import UIKit
+import Result
+
+public final class LoadingPlugin: PluginType {
+    
+    public init() {}
+    
+    public func willSend(_ request: RequestType, target: TargetType) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        guard let window = UIApplication.shared.keyWindow else { return }
+        window.displayLoading()
+    }
+    
+    public func didReceive(_ result: Result<Moya.Response, MoyaError>, target: TargetType) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        
+        guard let window = UIApplication.shared.keyWindow else { return }
+        window.hideLoading()
+    }
+}
