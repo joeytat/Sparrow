80969d3c527321296c507c8019d9c954a340b1fc
Joeytat <joeyqaq@gmail.com>
2018-04-10 15:43:53 +0800

    DKImagePickerController - add highQualityFormat option for load: method;

--- Sparrow/Classes/Extensions/DKImagePickerController+Extension.swift
+++ Sparrow/Classes/Extensions/DKImagePickerController+Extension.swift
@@ -8,6 +8,7 @@
 import DKImagePickerController
 import RxSwift
 import UIKit
+import Photos
 
 public extension Reactive where Base: DKImagePickerController {
     public static func present(selectedItems: [DKAsset] = [], limit: Int = 9) -> Observable<[DKAsset]> {
@@ -15,11 +16,11 @@ public extension Reactive where Base: DKImagePickerController {
             let vc = DKImagePickerController()
             let visibleVC = UIApplication.shared.keyWindow?.visibleViewController
             visibleVC?.present(vc, animated: true, completion: nil)
-            
+
             vc.assetType = .allPhotos
             vc.defaultSelectedAssets = selectedItems
             vc.maxSelectableCount = limit
-            
+
             vc.didCancel = {[unowned vc] in
                 observer.onNext(vc.selectedAssets)
                 observer.onCompleted()
@@ -39,14 +40,16 @@ public extension Reactive where Base: DKAsset {
         return Observable<(id:String, image: UIImage)>
             .create { observer in
                 let scaledSize = CGSize(width: size.width * UIScreen.main.scale, height: size.height * UIScreen.main.scale)
-                self.base.fetchImageWithSize(scaledSize) { (image, _) in
+                let option = PHImageRequestOptions()
+                option.deliveryMode = PHImageRequestOptionsDeliveryMode.highQualityFormat
+                self.base.fetchImageWithSize(scaledSize, options: option) { (image, _) in
                     observer.onNext((localId, image!))
                     observer.onCompleted()
                 }
                 return Disposables.create()
             }
     }
-    
+
     public func origin() -> Observable<(id:String, image: UIImage)> {
         let localId = self.base.localIdentifier
         return Observable<(id:String, image: UIImage)>
